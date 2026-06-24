fd64ef6cce80870050d00314702c9913861d6d3e
abdulwahabbinder-a11y <abdulwahabbinder@gmail.com>
2026-06-24 13:21:59 +0500

    Modify output setting and add comment in next.config.js
    Updated the output configuration and added a comment.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: "export", // Yeh lazmi 'export' hona chahiye taake 'out' folder bane!
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**.supabase.co" },
